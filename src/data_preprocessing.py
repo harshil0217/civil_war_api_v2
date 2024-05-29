30d5cba28799a8610d945eaca93904aa47f7ca0a
harshil0217 <harshil0217@gmail.com>
2024-05-29 18:04:40 -0400

    chore: Refactor data preprocessing code and add data imputation

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,29 +1,35 @@
 import pandas as pd
+import boto3
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from imblearn.under_sampling import RandomUnderSampler
+from sklearn.impute import SimpleImputer
 
 #loads data from path
-def load_data(file): 
-    data = pd.read_csv(file)
-    return data
+def load_data(): 
+    s3_client = boto3.client('s3', region_name='us-east-2')
+    bucket_name = 'harshil-storage'
+    file_key = 'v_dem_indicators_2024.csv'
+    response = s3_client.get_object(Bucket=bucket_name, Key=file_key)
+    indicators = pd.read_csv(response.get("Body"))
+    return indicators
 
 #dropping uneeded columns and rows
 def clean_data(indicators):
-    indicators = indicators.drop(columns=['country_name', 'country_id', 'histname' ,'historical_date','project','historical',
+    indicators.drop(columns=['country_name', 'country_id', 'histname' ,'historical_date','project','historical',
                          'codingstart','codingend', 'codingstart_contemp', 'codingend_contemp',
                          'codingstart_hist', 'codingend_hist', 'gapstart1', 
                          'gapstart2', 'gapstart3', 'gapend1', 'gapend2', 'gapend3', 'gap_index',
                          'COWcode'], inplace=True)
-    indicators = indicators[indicators.year >= 1980]
+    indicators = indicators[indicators["year"] >= 1980]
     indicators = indicators.sort_values(by=['country_text_id', 'year'])
-    indicators_2023 = indicators[indicators.year == 2023]
+    indicators_2023 = indicators[indicators["year"] == 2023]
     civil_war = indicators['e_civil_war']
     indicators.drop(columns=['e_civil_war'], inplace=True)
     indicators.insert(2, 'e_civil_war', civil_war)
     indicators = indicators.dropna(subset=['e_civil_war'])
-    indicators = indicators.dropna(thresh = indicators.shape[0]*0.3, axis = 1)
+    indicators = indicators.dropna(thresh = indicators.shape[0]*0.95, axis = 1)
 
     #dropping columns with codelow or codehigh in name
     indicators = indicators[indicators.columns.drop(list(indicators.filter(regex='codelow')))]
@@ -41,20 +47,6 @@ def clean_data(indicators):
     #dropping columns with osp in name
     indicators = indicators[indicators.columns.drop(list(indicators.filter(regex='osp')))]
 
-    #getting list of columns with nr in name
-    nr_columns = list(indicators.filter(regex='nr'))
-
-    #getting subset of nr columns
-    nr_indicators = indicators[nr_columns]
-
-    #replacing values less than 3 with NaN
-    nr_indicators.replace({3:None}, inplace=True)
-
-    #dropping rows with NaN
-    nr_indicators.dropna(axis=0, inplace=True)
-
-    #only selecting rows that are in the nr_indicators
-    indicators = indicators[indicators.index.isin(nr_indicators.index)]
 
     #dropping columns with nr in name
     indicators = indicators[indicators.columns.drop(list(indicators.filter(regex='nr')))]
@@ -78,7 +70,7 @@ def clean_data(indicators):
     X = indicators.drop(columns=['e_civil_war'])
     y = indicators['e_civil_war']
 
-    #dropping columns in the 2023 data that are more than 30% empty
+    #dropping columns in the 2023 data that have NaN
     indicators_2023 = indicators_2023.dropna(thresh=indicators_2023.shape[0]*0.3, axis=1)
 
     #removing columns in X that are not in the 2023 data
@@ -87,31 +79,39 @@ def clean_data(indicators):
 
     return X, y
 
+
+def impute_data(X):
+    imputer = SimpleImputer(strategy='mean')
+    X = imputer.fit_transform(X)
+    return X
+
+
 def scale_data(X):
     scaler = StandardScaler()
     X = scaler.fit_transform(X)
     return X
 
 def apply_pca(X):
-    pca = PCA(n_components=45)
+    pca = PCA(n_components=30)
     X = pca.fit_transform(X)
     return X
-
-def undersample_data(X, y):
-    rus = RandomUnderSampler(random_state=69)
-    X, y = rus.fit_resample(X, y)
-    return X, y
 
 def split_data(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=69)
     return X_train, X_test, y_train, y_test
 
-def preprocess_data(file):
-    indicators = load_data(file)
+def undersample_data(X_train, y_train):
+    rus = RandomUnderSampler(random_state=69)
+    X_train, y_train = rus.fit_resample(X_train, y_train)
+    return X_train, y_train
+
+def preprocess_data():
+    indicators = load_data()
     X, y = clean_data(indicators)
+    X = impute_data(X)
     X = scale_data(X)
     X = apply_pca(X)
-    X, y = undersample_data(X, y)
     X_train, X_test, y_train, y_test = split_data(X, y)
+    X_train, y_train = undersample_data(X_train, y_train)
     return X_train, X_test, y_train, y_test
 
---
+++ src/model_training.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+from data_preprocessing import preprocess_data
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Input
+import tensorflow as tf
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
+import matplotlib.pyplot as plt
+
+#load the preprocessed data
+X_train, X_test, y_train, y_test = preprocess_data()
+
+#creating sequential model
+model = Sequential()
+model.add(Input(shape=(X_train.shape[1],)))
+model.add(Dense(20, activation='relu'))
+model.add(Dense(20, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+#creating optimizer
+adam = keras.optimizers.Adam(learning_rate=0.001)
+
+#compile the model
+model.compile(loss='binary_crossentropy', optimizer=adam, metrics=["accuracy"])
+
+#train the model
+model.fit(X_train, y_train, epochs=100)
+
+#evaluate the model
+loss_and_metrics = model.evaluate(X_test, y_test)
+print(loss_and_metrics)
+print('Loss = ',loss_and_metrics[0])
+print('Accuracy = ',loss_and_metrics[1])
+
+#see predictions
+preds = model.predict(X_test)
+preds = np.round(preds)
+cm = confusion_matrix(y_test, preds)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+disp.plot()
+plt.savefig('./images/confusion_matrix.png')
+print(classification_report(y_test, preds))
